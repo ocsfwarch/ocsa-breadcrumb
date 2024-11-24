5c60bb23107b53be552052b3583c25b0c9de0cbd
ocsfwarch <tpartcal@gmail.com>
2024-11-24 09:40:41 -0800

    Adds optional onClick handler

--- src/OcsaBreadcrumb.tsx
+++ src/OcsaBreadcrumb.tsx
@@ -9,21 +9,28 @@ export interface BreadcrumbItem {
 
 export interface OcsaBreadcrumbProps {
   items: BreadcrumbItem[];
+  onClickHandler?: (item: BreadcrumbItem) => void;
 }
 
-const OcsaBreadcrumb = ({ items }: OcsaBreadcrumbProps) => {
+const OcsaBreadcrumb = ({ items, onClickHandler }: OcsaBreadcrumbProps) => {
   return (
     <nav aria-label="breadcrumb" className="breadcrumb">
       <ul>
-        {items.map((item, index) => (
-          <li key={index}>
-            {item.isCurrent ? (
+        {items.map((item, index) =>
+          item.isCurrent ? (
+            <li key={index}>
               <span aria-current="page">{item.label}</span>
-            ) : (
-              <a href={item.href}>{item.label}</a>
-            )}
-          </li>
-        ))}
+            </li>
+          ) : (
+            <li key={index} onClick={() => onClickHandler?.(item)}>
+              {onClickHandler !== undefined ? (
+                <span className="link">{item.label}</span>
+              ) : (
+                <a href={item.href}>{item.label}</a>
+              )}
+            </li>
+          )
+        )}
       </ul>
     </nav>
   );
